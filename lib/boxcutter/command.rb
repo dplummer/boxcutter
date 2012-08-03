40c7fe119602c7fde54371516ee8e9926695aea4
Michael Xavier <michael@michaelxavier.net>
2012-08-02 20:32:15 -0700

    Add spec for add_machine

--- lib/boxcutter/command.rb
+++ lib/boxcutter/command.rb
@@ -44,21 +44,21 @@ module Boxcutter
       dryrun       = opts.fetch(:dryrun, false)
       hostname     = opts.fetch(:hostname)
 
-      if machine = Boxcutter::Server.find_by_hostname("#{hostname}.blueboxgrid.com")
+      if server = Boxcutter::Server.find_by_hostname("#{hostname}.blueboxgrid.com")
 
         app = Boxcutter::LoadBalancer::Application.all.first
 
         app.services.each do |service|
           if backend = service.backends.detect {|backend| backend.name == backend_name}
-            log "Adding machine #{machine.hostname} to backend #{backend.name}"
+            log "Adding server #{server.hostname} to backend #{backend.name}"
             unless dryrun
-              response = backend.add_machine(machine.id, :port => 80)
-              log "Added #{machine.hostname} to #{backend.name} with response: #{response.body}"
+              response = backend.add_server(server.id, :port => 80)
+              log "Added #{server.hostname} to #{backend.name} with response: #{response.body}"
             else
-              log "#{machine.hostname} was not added to the backend because --dryrun was specified"
+              log "#{server.hostname} was not added to the backend because --dryrun was specified"
             end
           else
-            log "Could not find default backend on #{service}"
+            log "Could not find '#{backend_name}' backend on '#{service.name}'"
           end
         end
       else
--- spec/boxcutter/command_spec.rb
+++ spec/boxcutter/command_spec.rb
@@ -5,15 +5,22 @@ module Boxcutter
   describe Command do
     let(:logger) { StringIO.new }
     let(:remove_response) { stub(:body => "AW YEAH") }
-    let(:machine) { stub("Machine", :remove! => remove_response, :hostname => 'app1') }
+    let(:add_response) { stub(:body => "YEEUH") }
+    let(:machine) { stub("Machine",
+                         :remove!     => remove_response,
+                         :hostname => 'app1',
+                         :id => "MACHINE1") }
     let(:other_machine) { stub("Other Machine", :hostname => 'badapp2')}
     let(:machines) { [machine, other_machine] }
     let(:backends) { [backend, other_backend] }
     let(:other_backend) { stub("Other Backend", :name => 'foo')}
-    let(:backend) { stub("Backend", :name => 'default', :machines => machines)}
+    let(:backend) { stub("Backend",
+                         :name => 'default',
+                         :add_server => add_response,
+                         :machines => machines)}
     let(:app) { stub("App", :services => services) }
     let(:services) { [service] }
-    let(:service) { stub("Service", :backends => backends) }
+    let(:service) { stub("Service", :name => "HTTP", :backends => backends) }
 
     subject { Command.new(logger) }
 
@@ -95,6 +102,76 @@ module Boxcutter
           expect do
             subject.remove_machine
           end.to raise_error(IndexError)
+        end
+      end
+    end
+
+    describe "#add_machine" do
+      let(:hostname)    { "app1" }
+      let(:backend_opt) { "default" }
+      let(:dryrun)      { false }
+      let(:opts) {{
+        :backend => backend_opt,
+        :dryrun  => dryrun,
+        :hostname => hostname
+      }}
+      let(:server) { stub("Server", :hostname => 'app1', :id => "APP1") }
+
+      before(:each) do
+        Boxcutter::Server.stub(:find_by_hostname).
+                          with("app1.blueboxgrid.com").
+                          and_return(server)
+      end
+
+      it "logs the server add" do
+        subject.add_machine(opts)
+
+        logger_contents.should include("Adding server app1 to backend default")
+      end
+
+      it "adds the machine by id, using port 80" do
+        backend.should_receive(:add_server).with('APP1', :port => 80)
+        subject.add_machine(opts)
+      end
+
+      it "logs the server add" do
+        subject.add_machine(opts)
+
+        logger_contents.should include("Adding server app1 to backend default")
+      end
+
+      context "dry run" do
+        let(:dryrun) { true }
+
+        it "does not add the server to the backend" do
+          backend.should_not_receive(:add_server)
+          subject.add_machine(opts)
+        end
+
+        it "instead logs the add" do
+          subject.add_machine(opts)
+
+          logger_contents.should include("app1 was not added to the backend because --dryrun was specified")
+        end
+      end
+
+      context "backend is not found" do
+        let(:backends) { [other_backend] }
+
+        it "logs the error" do
+          subject.add_machine(opts)
+
+          logger_contents.should include("Could not find 'default' backend on 'HTTP'")
+        end
+      end
+
+      context "server is not found" do
+        let(:server) { nil }
+
+        it "logs the error" do
+          subject.add_machine(opts)
+
+          logger_contents.should include("Could not find server 'app1' on BlueBoxGroup")
         end
       end
     end
